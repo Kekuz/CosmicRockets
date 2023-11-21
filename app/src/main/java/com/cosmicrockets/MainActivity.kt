740fe6922f1a46f8455e7239a234c7349139dfbd
Kekuz <anton.edlenko@yandex.ru>
2023-11-22 01:49:09 +0300

    View pager and Tab Layout is done

--- app/src/main/java/com/cosmicrockets/MainActivity.kt
+++ app/src/main/java/com/cosmicrockets/MainActivity.kt
@@ -2,10 +2,30 @@ package com.cosmicrockets
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import com.cosmicrockets.databinding.ActivityMainBinding
+import com.google.android.material.tabs.TabLayoutMediator
 
 class MainActivity : AppCompatActivity() {
+
+    //Чтобы получить этот список, мы дерзко идем в сеть и узнаем, какие у нас есть ракеты
+    private val fragList = listOf(
+        RocketFragment(),
+        RocketFragment(),
+        RocketFragment(),
+    )
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val viewPagerAdapter = ViewPagerAdapter(this, fragList)
+        binding.viewPager.adapter = viewPagerAdapter
+        TabLayoutMediator(
+            binding.tabLayout,
+            binding.viewPager
+        ) { _, _ -> //tab.text = fragListTitles[pos]
+        }.attach()
     }
 }
---
+++ app/src/main/java/com/cosmicrockets/ViewPagerAdapter.kt
@@ -0,0 +1,15 @@
+package com.cosmicrockets
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager2.adapter.FragmentStateAdapter
+
+class ViewPagerAdapter(fragmentActivity: FragmentActivity, private val list: List<RocketFragment>) : FragmentStateAdapter(fragmentActivity) {
+    override fun getItemCount(): Int {
+        return list.size
+    }
+
+    override fun createFragment(position: Int): Fragment {
+        return list[position]
+    }
+}
