8683db51bb9f3bdda4ce1261ed0335902e464341
Kekuz <anton.edlenko@yandex.ru>
2023-11-21 23:10:38 +0300

    Init commit

---
+++ app/src/main/java/com/cosmicrockets/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.cosmicrockets
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
