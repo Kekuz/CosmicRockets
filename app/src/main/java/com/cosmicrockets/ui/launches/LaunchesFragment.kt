2e9a9d049df57852cdabb8dbdb75fff333b14d0a
Kekuz <anton.edlenko@yandex.ru>
2023-11-24 01:45:14 +0300

    added animation to opening launches fragment, launches fragment ui

--- app/src/main/java/com/cosmicrockets/ui/launches/LaunchesFragment.kt
+++ app/src/main/java/com/cosmicrockets/ui/launches/LaunchesFragment.kt
@@ -5,21 +5,33 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.cosmicrockets.R
+import com.cosmicrockets.databinding.FragmentLaunchesBinding
+import com.cosmicrockets.databinding.FragmentRocketBinding
+import com.cosmicrockets.presentation.models.RocketInfo
 
 
 class LaunchesFragment : Fragment() {
+    private lateinit var binding: FragmentLaunchesBinding
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-    }
-
+    private var id : String? = null
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        binding = FragmentLaunchesBinding.inflate(inflater, container, false)
 
-        return inflater.inflate(R.layout.fragment_launches, container, false)
+        id = requireArguments().getString("id")
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.rocketNameTv.text = requireArguments().getString("name")
+        binding.backBtn.setOnClickListener {
+            findNavController().popBackStack()
+        }
     }
 }
